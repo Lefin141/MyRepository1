dcfb538fce3ee4c3fe9acc55d0538ea7a54464a2
lefin <krlefin@gmail.com>
2020-03-02 19:57:55 +0530

    My Commit

---
+++ yftf7t/src/yftf7t/any.java
@@ -0,0 +1,34 @@
+package yftf7t;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class any {
+
+	public static void main(String[] args) {
+		 
+String atr="Helloworld";
+String rev="";
+
+int length=atr.length();
+
+
+
+for(int i=length-1;i>=0;i--) {
+	
+System.out.println();
+	rev=rev+atr.charAt(i);
+	
+	
+}
+
+System.out.println(rev);
+
+
+
+
+}
+}
